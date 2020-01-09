2a422eec9c3cf4809eaf0c317000d847e9c65e0d
Krittanut Jirathawong <j.krittanut@gmail.com>
2020-01-09 20:20:17 +0700

    refactor the model to an object to be serializable

--- model_dyn.py
+++ model_dyn.py
@@ -1,43 +1,42 @@
-# parameters
-prices = [1.99, 2.49, 2.99, 3.49, 3.99, 4.49]
-alpha_0 = 30.00     # parameter of the prior distribution
-beta_0 = 1.00       # parameter of the prior distribution
+import numpy as np
 
-# parameters of the true (unknown) demand model
-true_slop = 50
-true_intercept = -7
+T = 100
 
-# prior distribution for each price
-p_theta = []
-for p in prices:
-    p_theta.append({'price': p, 'alpha': alpha_0, 'beta': beta_0})
+class DynamicPricingModel:
+    # parameters
+    prices = [1.99, 2.49, 2.99, 3.49, 3.99, 4.49]
+    alpha_0 = 30.00     # parameter of the prior distribution
+    beta_0 = 1.00       # parameter of the prior distribution
+    p_theta = []
+    price_index_offered = 0
 
-def sample_actual_demand(price): 
-    demand = true_slop + true_intercept * price
-    return np.random.poisson(demand, 1)[0]
+    def __init__(self):
+        for p in self.prices:
+            self.p_theta.append({'price': p, 'alpha': self.alpha_0, 'beta': self.beta_0})
+    
+    def sample_demands_from_model(self, p_theta):
+        return list(map(lambda v: 
+                np.random.gamma(v['alpha'], 1/v['beta']), self.p_theta))
+    
+    def optimal_price(self):
+        demands = self.sample_demands_from_model(self.p_theta)
 
-# sample mean demands for each price level
-def sample_demands_from_model(p_theta):
-    return list(map(lambda v: 
-           np.random.gamma(v['alpha'], 1/v['beta']), p_theta))
+        print("demands:", ["%.2f"%demand for demand in demands])
 
-# return price that maximizes the revenue
-def optimal_price(prices, demands):
-    price_index = np.argmax(np.multiply(prices, demands))
-    return price_index, prices[price_index]
+        price_index = np.argmax(np.multiply(self.prices, demands))
+        self.price_index_offered = price_index
+        return price_index, self.prices[price_index]
 
-# simulation loop        
+    def update(self, demand_t):
+        demand_t = int(demand_t)
+        v = self.p_theta[self.price_index_offered]
+        v['alpha'] = v['alpha'] + demand_t
+        v['beta'] = v['beta'] + 1
+
+model = DynamicPricingModel()
+
 for t in range(0, T):
     print("Iteration", t)
-    demands = sample_demands_from_model(p_theta)
-    print("demands:", ["%.2f"%demand for demand in demands])
-    price_index_t, price_t = optimal_price(prices, demands)
-    print("optimal price:", price_t)
-    
-    # offer the selected price and observe demand
-    demand_t = sample_actual_demand(price_t)
-        
-    # update model parameters
-    v = p_theta[price_index_t]
-    v['alpha'] = v['alpha'] + demand_t
-    v['beta'] = v['beta'] + 1
+    optimal_price_index, optimal_price = model.optimal_price()
+    print("optimal price: ", optimal_price)
+    model.update(input("Demand of "  + str(optimal_price) + ": "))
